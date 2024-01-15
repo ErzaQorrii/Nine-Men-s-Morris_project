f9c5b5e5f9daf40e31e8572615b9990f48cec466
Puhiza Tmava <94013520+PuhizaTmava@users.noreply.github.com>
2024-01-15 19:56:53 +0100

    SCRUM-62 Implemento inicializimin e lojtarit

--- view/place.js
+++ view/place.js
@@ -56,3 +56,74 @@ function Place (element, index) {
             piece.remove_slowly("little_slower");
         piece = null;
     };
+
+     
+    this.enable_remove = function (remove_piece_fnc) {
+        if ( !piece )
+            return;
+        element.style.cursor = "pointer";
+        piece.element.classList.add("spin");
+        click_function = function () {
+            element.style.cursor = "default";
+            piece.element.classList.remove("spin");
+            myself.disable_remove();
+            remove_piece_fnc(index, machine_colour);
+        };
+        element.addEventListener("touchstart", click_function, false);
+        element.addEventListener("mousedown", click_function, false);
+    };
+    this.disable_remove  = function () {
+        if ( piece )
+            piece.element.classList.remove("spin");
+        myself.disable_set();
+    };
+    
+    this.enable_set = function (set_piece_fnc) {
+        if ( piece )
+            return;
+        element.style.cursor = "pointer";
+        click_function = function () {
+            element.style.cursor = "default";
+            myself.disable_set();
+            set_piece_fnc(index, human_colour);
+        };
+        element.addEventListener("touchstart", click_function, false);
+        element.addEventListener("mousedown", click_function, false);
+    };
+    this.disable_set = function () {
+        element.style.cursor = "default";
+        if ( click_function ) {
+            element.removeEventListener("touchstart", click_function, false);
+            element.removeEventListener("mousedown", click_function, false);
+        }
+        click_function = null;
+    };
+    
+    this.enable_move = function (targets, move_piece_fnc) {
+        if ( !piece )
+            return;
+        myself.disable_set();
+        piece.initialise_move(index, targets, move_piece_fnc);
+    };
+    this.disable_move = function () {
+        if ( !piece )
+            return;
+        piece.disable_move();
+    };
+    this.move = function (target) {
+        if ( !piece )
+            return;
+        target.slide_piece(myself, piece);
+        piece = null;
+    };
+    
+    this.set_play_mode = function () {
+        if ( !piece )
+            return;
+    };
+    
+    area = document.createElement("div");
+    area = document.createElement("div");
+    area.className = "place_area";
+    element.appendChild(area);
+}
