154186e45059a4767837e1e95f9543a0baa66a95
Arbreshass <93888146+Arbreshass@users.noreply.github.com>
2024-01-16 20:51:14 +0100

    SCRUM-40 Shto nje funksion per gjenerimin e figurave

--- view/board.js
+++ view/board.js
@@ -7,3 +7,39 @@ function generate_pieces (colour) {
     
     return pieces;
 }
+function generate_places () {
+   
+    var places = [];
+
+    var parent_element = document.getElementById("base");
+    var element, v, h, r;
+    
+    var s = [1, 1, 1, 4, 7, 7, 7, 4];
+
+
+    for ( var i = 0; i < 24; i++ ) {
+       
+        element = document.createElement("div");
+        
+        r = parseInt(i / 8);
+     
+        v = s[i % 8];
+      
+        h = s[(i + 2) % 8];
+    
+        if ( r ) {
+
+            if ( v != 4 )
+                v = (v < 4) ? (v + r) : (v - r);
+            if ( h != 4 )
+                h = (h < 4) ? (h + r) : (h - r);
+        }
+        
+        element.className = "place v" + v + " h" + h;
+        
+        places[i] = new Place(element, i);
+        parent_element.appendChild(element);
+    }
+    
+    return places;
+}
