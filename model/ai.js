d80c0bc64164788d895bab83d2d10dd14087a1d1
ErzaQorri <93944366+ErzaQorrii@users.noreply.github.com>
2024-01-18 20:34:47 +0100

    Add set_piece logic and implement onmessage handler for game actions

--- model/ai.js
+++ model/ai.js
@@ -28,6 +28,18 @@ function randomise_indices () {
     }
 }
 randomise_indices();
+function push_position_to_front (position, index) {
+    index = index || 0;
+    var tmp, i;
+    for ( i = (index + 1); i < 24; i++ ) {
+        if ( indices[i] == position ) {
+            indices[i] = indices[index];
+            indices[index] = position;
+            break;
+        }
+    }
+}
+
 var Mills = Array(24);
 var AlmostMills = Array(24);
 var tmp, tmp1, tmp2, tmp3, ni, n, na;
@@ -68,7 +80,6 @@ for ( i = 0; i < 24; i++ ) {
     AllNeighbours[ni] = na;
 }
 
-
 function convert (board) {
     var white_board = 0;
     var black_board = 0;
@@ -97,114 +108,39 @@ function convert (board) {
 
 /*  */
 
-// Function to check if building a mill is possible on the board at a given index.
-function builds_mill(board, index) {
+function builds_mill (board, index) {
     var mill;
-
-    // Loop through all possible mill configurations for the given index.
-    for (var i = 0; i < Mills[index].length; i++) {
-        mill = Mills[index][i]; // Get the current mill configuration
-
-        // Check if the current mill configuration is present on the board.
-        // The bitwise AND operation is used to compare the board state with the mill configuration.
-        if ((mill & board) == mill)
-            return true; // Return true if the current configuration forms a mill on the board.
+    for ( var i = 0; i < Mills[index].length; i++ ) {
+        mill = Mills[index][i];
+        if ( (mill & board) == mill )
+            return true;
     }
-
-    return false; // Return false if no mill configuration is formed at the given index.
+    return false;
 }
-
-
-// Function to check if a player is almost building a mill on the board.
-function almost_builds_mill(enemy_board, joined_board, index) {
-    // Retrieve possible mill and almost mill configurations for the given index.
+function almost_builds_mill (enemy_board, joined_board, index) {
     var mills = Mills[index];
     var almost_mills = AlmostMills[index];
-
-    // Initialize the result array to track the status of potential mills.
     var result = [0, 0];
-
-    // Check for the first potential mill configuration.
-    if (!(mills[0] & enemy_board) && (mills[0] & joined_board)) {
-        // If the enemy hasn't formed the mill but the joined board has the configuration, set result to 1.
+    var i;
+    if ( !(mills[0] & enemy_board) && (mills[0] & joined_board) ) {
         result[0] = 1;
-
-        // Check if the player is almost forming a mill.
-        for (var i = 0; i < 3; i++) {
-            if ((almost_mills[i] & joined_board) == almost_mills[i]) {
-                // If an almost mill is formed, update the result to 2 and exit the loop.
+        for ( i = 0; i < 3; i++ ) {
+            if ( (almost_mills[i] & joined_board) == almost_mills[i] ) {
                 result[0] = 2;
                 break;
             }
         }
     }
-
-    // Repeat the check for the second potential mill configuration.
-    if (!(mills[1] & enemy_board) && (mills[1] & joined_board)) {
-        result[1] = 1; // Similar logic as above for the second configuration.
-        
-        // Loop through the next set of almost mill configurations.
-        for (var i = 3; i < 6; i++) {
-            if ((almost_mills[i] & joined_board) == almost_mills[i]) {
-                result[1] = 2; // Update the result if an almost mill is found.
+    if ( !(mills[1] & enemy_board) && (mills[1] & joined_board) ) {
+        result[1] = 1;
+        for ( i = 3; i < 6; i++ ) {
+            if ( (almost_mills[i] & joined_board) == almost_mills[i] ) {
+                result[1] = 2;
                 break;
             }
         }
     }
-
-    // Return the result array indicating the status of potential mills.
     return result;
-}
-
-// Function to determine the best move for a piece in a board game.
-function move_piece(colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces) {
-
-    var MAX = 6; // Define a constant MAX used for move depth or analysis intensity.
-
-    var result;
-    // Call a helper function to evaluate a move, initially with a depth of 2.
-    var tmp_result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, 2);
-
-    // Check the quality of the returned move, comparing it against a threshold (13247).
-    if (tmp_result && tmp_result.length > 1 && tmp_result[1] && tmp_result[1].length > 1 && Math.abs(tmp_result[0]) > 13247)
-        result = tmp_result;
-    else {
-        // If aggressive analysis is enabled, try a different strategy with depth 1.
-        if (agressive_analyse) {
-            tmp_result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, 1);
-            if (tmp_result && tmp_result.length > 1 && tmp_result[1] && tmp_result[1].length > 1)
-                push_position_to_front(tmp_result[1][0]); // Prioritize this move.
-        }
-        // Make a final attempt to determine a move, using the maximum depth or intensity.
-        result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, MAX);
-    }
-
-    // Log the quality and details of the chosen move, if available.
-    if (result)
-        console.log("MOVE quality: " + result[0]);
-    if (result && result.length > 1 && result[1] && result[1].length > 1)
-        console.log("To move: " + result[1][0] + " --> " + result[1][1]);
-
-    // If no valid move is found, log a message and prepare to give up.
-    if (!result || result.length < 2 || !result[1] || result[1].length < 2) {
-        console.log("Machine gives up in move_piece");
-        var msg = {
-            task: "give_up",
-            id: callback_id // Use a callback identifier for the message.
-        };
-    }
-    else {
-        // If a valid move is found, prepare a message with the move details.
-        var msg = {
-            task: "move_piece",
-            from: result[1][0],
-            to: result[1][1],
-            id: callback_id
-        };
-    }
-
-    // Send the message (either a move or give up).
-    postMessage(msg);
 }
 
 var agressive_analyse = null;
@@ -339,6 +275,7 @@ function analyse (white_board, black_board) {
 }
 
 /*  */
+
 function jump_piece (colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces) {
     
     var MAX = 4;
@@ -375,8 +312,6 @@ function jump_piece (colour, white_board, black_board, white_pieces, black_piece
 
     postMessage(msg);
 }
-
-
 function remove_piece (colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces) {
     
     var MAX = 4;
@@ -405,3 +340,104 @@ function remove_piece (colour, white_board, black_board, white_pieces, black_pie
 
     postMessage(msg);
 }
+function move_piece (colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces) {
+
+    var MAX = 6;
+
+    var result;
+    var tmp_result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, 2);
+    if ( tmp_result && tmp_result.length > 1 && tmp_result[1] && tmp_result[1].length > 1 && Math.abs(tmp_result[0]) > 13247 )
+        result = tmp_result;
+    else {
+        if ( agressive_analyse ) {
+            tmp_result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, 1);
+            if ( tmp_result && tmp_result.length > 1 && tmp_result[1] && tmp_result[1].length > 1 )
+                push_position_to_front(tmp_result[1][0]);
+        }
+        result = _move_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, MAX);
+    }
+
+    if ( result )
+        console.log( "MOVE quality: " + result[0] );
+    if ( result && result.length > 1 && result[1] && result[1].length > 1 )
+        console.log( "To move: " + result[1][0] + " --> " + result[1][1] );
+
+    if ( !result || result.length < 2 || !result[1] || result[1].length < 2 ) {
+        console.log( "Machine gives up in move_piece" );
+        var msg = {
+            task: "give_up",
+            id: callback_id
+        };
+    }
+    else {
+        var msg = {
+            task: "move_piece",
+            from: result[1][0],
+            to: result[1][1],
+            id: callback_id
+        };
+    }
+
+    postMessage(msg);
+}
+
+function set_piece (colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces) {
+    
+    var MAX = 4;
+
+    var result = _set_piece(null, colour, white_board, black_board, white_pieces, black_pieces, initial_white_pieces, initial_black_pieces, MAX);
+
+    if ( result )
+        console.log( "SET quality: " + result[0] );
+    if ( result && result.length > 1 && result[1] && result[1].length > 1 )
+        console.log( "Place to set: " + result[1][1] );
+
+    if ( !result || result.length < 2 || !result[1] || result[1].length < 2 ) {
+        console.log( "Machine gives up in set_piece" );
+        var msg = {
+            task: "give_up",
+            id: callback_id
+        };
+    }
+    else {
+        var msg = {
+            task: "set_piece",
+            place: result[1][1],
+            id: callback_id
+        };
+    }
+
+    postMessage(msg);
+}
+
+onmessage = function (event) {
+    
+    callback_id = event.data.id;
+    var colour = event.data.moving;
+    
+    var converted = convert(event.data.board);
+    
+    var fnc = (event.data.remove) ? remove_piece : null;
+    if ( !fnc ) {
+        if ( event.data.initial_pieces[colour_indices[colour]] )
+            fnc = set_piece;
+        else
+            fnc = (converted.pieces[colour] <= 3) ? jump_piece : move_piece;
+    }
+
+    var other_colour = (colour == "black") ? "white" : "black";
+    if ( fnc == move_piece && converted.pieces[colour] > converted.pieces[other_colour] )
+        agressive_analyse = colour;
+    
+    execution_counter = 0;
+    analise_counter = 0;
+    var start_time = (new Date()).getTime();
+    
+    fnc(colour, converted.white_board, converted.black_board, converted.pieces.white, converted.pieces.black, event.data.initial_pieces[0], event.data.initial_pieces[1]);
+    
+    var end_time = (new Date()).getTime();
+    console.log( "Execution duration: " + ((end_time - start_time) / 1000) );
+    
+    setTimeout(randomise_indices, 17);
+    agressive_analyse = null;
+};
